50c1e90c4b6eeb7824101e6689646e8f456f3a94
johnmccann1991 <john.mccann@kellogg.northwestern.edu>
2022-01-25 02:22:39 +0000

    Data Entry

--- kmdb.sql
+++ kmdb.sql
@@ -95,7 +95,7 @@
 -- TODO!
 
 DROP TABLE IF EXISTS movies ;
-DROP TABLE IF EXISTS top_cast;
+DROP TABLE IF EXISTS top_cast ;
 
 CREATE TABLE movies (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -111,5 +111,61 @@ CREATE TABLE top_cast (
     name TEXT,
     role TEXT,
 );
+
+INSERT INTO movies (title, year, mpaa_rating, director)
+VALUES ("Batman Begins", 2005, "PG-13", "Christopher Nolan");
+INSERT INTO movies (title, year, mpaa_rating, director)
+VALUES ("The Dark Knight", 2008, "PG-13", "Christopher Nolan");
+INSERT INTO movies (title, year, mpaa_rating, director)
+VALUES ("The Dark Knight Rises", 2012, "PG-13", "Christopher Nolan");
+
+INSERT INTO top_cast (title, name, role)
+VALUES ("Batman Begins", "Christian Bale", "Bruce Wayne");
+INSERT INTO top_cast (title, name, role)
+VALUES ("Batman Begins", "Michael Caine", "Alfred");
+INSERT INTO top_cast (title, name, role)
+VALUES ("Batman Begins", "Liam Neeson", "Ra's Al Ghul");
+INSERT INTO top_cast (title, name, role)
+VALUES ("Batman Begins", "Katie Holmes", "Rachel Dawes");
+INSERT INTO top_cast (title, name, role)
+VALUES ("Batman Begins", "Gary Oldman", "Commisioner Gordon");
+INSERT INTO top_cast (title, name, role)
+VALUES ("The Dark Knight", "Christian Bale", "Bruce Wayne");
+INSERT INTO top_cast (title, name, role)
+VALUES ("The Dark Knight", "Heath Ledger", "Joker");
+INSERT INTO top_cast (title, name, role)
+VALUES ("The Dark Knight", "Aaron Eckhart", "Harvey Dent");
+INSERT INTO top_cast (title, name, role)
+VALUES ("The Dark Knight", "Michael Caine", "Alfred");
+INSERT INTO top_cast (title, name, role)
+VALUES ("The Dark Knight", "Maggie Gyllenhaal", "Rachel Dawes");
+INSERT INTO top_cast (title, name, role)
+VALUES ("The Dark Knight Rises", "Christian Bale", "Bruce Wayne");
+INSERT INTO top_cast (title, name, role)
+VALUES ("The Dark Knight Rises", "Gary Oldman", "Commisioner Gordon");
+INSERT INTO top_cast (title, name, role)
+VALUES ("The Dark Knight Rises", "Tom Hardy", "Bane");
+INSERT INTO top_cast (title, name, role)
+VALUES ("The Dark Knight Rises", "Joseph Gordon-Levitt", "John Blake");
+INSERT INTO top_cast (title, name, role)
+VALUES ("The Dark Knight Rises", "Anne Hathaway", "Selina Kyle");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
